16ab6d22dece2e82a04a7472f500a590d910d715
judos <judos@gmx.ch>
2015-09-04 17:23:05 +0200

    ships don't spawn near leaving ships (collisions which player can't prevent)

--- ShipMasterEclipseProject/src/controller/SpawnShipController.java
+++ ShipMasterEclipseProject/src/controller/SpawnShipController.java
@@ -5,6 +5,7 @@ import model.Map;
 import model.Ship;
 import model.ShipType;
 import ch.judos.generic.data.geometry.DirectedPoint;
+import ch.judos.generic.data.geometry.PointI;
 
 /**
  * @since 17.05.2015
@@ -13,6 +14,7 @@ import ch.judos.generic.data.geometry.DirectedPoint;
 public class SpawnShipController {
 
 	private static final float spawnShipEveryXSec = 7;
+	private static final int dontSpawnNearerThanXPixelsToShip = 200;
 
 	private Map map;
 
@@ -29,11 +31,34 @@ public class SpawnShipController {
 			this.timer = 0;
 
 			ShipType type = ShipType.getRandom();
-			DirectedPoint spawn = this.map.getSpawnLocation(type.getSize());
+			DirectedPoint spawn;
+			do {
+				spawn = this.map.getSpawnLocation(type.getSize());
+			} while (nearestDistanceToPoint(spawn.getPoint(), dontSpawnNearerThanXPixelsToShip) < dontSpawnNearerThanXPixelsToShip);
 			Cargo cargo = this.map.generateCargo(type);
 			Ship ship = new Ship(spawn, type, cargo);
 			this.map.addShip(ship);
 		}
+	}
+
+	/**
+	 * @param point
+	 *            the point from where to check the distance to the nearest ship
+	 * @param lowerLimitInterested
+	 *            if the value is below this limit, immediately returns
+	 * @return distance
+	 */
+	public int nearestDistanceToPoint(PointI point, int lowerLimitInterested) {
+		int distance = Integer.MAX_VALUE;
+		for (Ship s : this.map.getShips()) {
+			int currentDistance = (int) s.getPoint().distance(point);
+			// improve performance with this lower limit test
+			if (currentDistance < lowerLimitInterested)
+				return currentDistance;
+			if (currentDistance < distance)
+				distance = currentDistance;
+		}
+		return distance;
 	}
 
 }
